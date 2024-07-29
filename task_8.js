766f56cedc439d7d6e78d8d7b64047a12f1fd7f3
Neha-Miglani <123479469+Neha-Miglani@users.noreply.github.com>
2024-07-29 09:44:52 +0530

    Add files via upload

---
+++ task_8.js
@@ -0,0 +1,21 @@
+function createInstanceCounter() {
+    let count = 0;
+    return function() {
+        count += 1;
+        return count;
+    };
+}
+
+const counter1 = createInstanceCounter();
+const counter2 = createInstanceCounter();
+const counter3 = createInstanceCounter();
+
+console.log(counter1()); 
+console.log(counter1()); 
+console.log(counter2());
+console.log(counter2()); 
+console.log(counter3()); 
+console.log(counter3());
+console.log(counter3()); 
+console.log(counter3()); 
+
